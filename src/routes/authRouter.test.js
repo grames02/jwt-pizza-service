baa31c94eff0a8c337bab7c791c1d46ebb031fb3
grames02 <grames02@gmail.com>
2026-02-03 22:32:46 -0700

    another one

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,3 +1,41 @@
-test('hello world', () => {
-  expect('hello' + ' world').toBe('hello world');
+const request = require('supertest');
+const app = require('../service');
+
+const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+let testUserAuthToken;
+
+beforeAll(async () => {
+  testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
+  const registerRes = await request(app).post('/api/auth').send(testUser);
+  testUserAuthToken = registerRes.body.token;
+  expectValidJwt(testUserAuthToken);
 });
+
+test('login', async () => {
+  const loginRes = await request(app).put('/api/auth').send(testUser);
+  expect(loginRes.status).toBe(200);
+  expectValidJwt(loginRes.body.token);
+
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(loginRes.body.user).toMatchObject(expectedUser);
+});
+
+function expectValidJwt(potentialJwt) {
+  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
+}
+
+test('get current user', async () => {
+  const currentUserRes = await request(app)
+    .get('/api/auth/current')
+    .set('Authorization', `Bearer ${testUserAuthToken}`);
+  expect(currentUserRes.status).toBe(200);
+  const expectedUser = { ...testUser, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(currentUserRes.body).toMatchObject(expectedUser);
+});
+
+test('unauthorized get current user', async () => {
+  const currentUserRes = await request(app).get('/api/auth/current');
+  expect(currentUserRes.status).toBe(401);
+}); 
