e3beedc2bb7eeb8f6e17580966e06078eee4a56a
grames02 <grames02@gmail.com>
2026-02-04 00:35:29 -0700

    So it has to do with OrderRouter. Let's see is this works now.

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,8 +1,8 @@
+global.fetch = jest.fn();
+
 const request = require('supertest');
 const app = require('../service');
 const { Role, DB } = require('../database/database.js');
-
-global.fetch = jest.fn();
 
 function randomName() {
   return Math.random().toString(36).substring(2, 12);
