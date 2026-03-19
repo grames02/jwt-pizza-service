6a7086b4d6f3015990ddd924fe4f5cbcb9a48c1c
grames02 <grames02@gmail.com>
2026-03-19 01:54:32 -0600

    Deliverable 8

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -100,31 +100,6 @@ authRouter.authenticateToken = (req, res, next) => {
   next();
 };
 
-// register
-authRouter.post(
-  '/',
-  asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    if (!name || !email || !password) {
-      return res.status(400).json({ message: 'name, email, and password are required' });
-    }
-    const user = await DB.addUser({ name, email, password, roles: [{ role: Role.Diner }] });
-    const auth = await setAuth(user);
-    res.json({ user: user, token: auth });
-  })
-);
-
-// login
-authRouter.put(
-  '/',
-  asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const user = await DB.getUser(email, password);
-    const auth = await setAuth(user);
-    res.json({ user: user, token: auth });
-  })
-);
-
 // logout
 authRouter.delete(
   '/',
