303b0f90eabb9ceca80d492d85921c66fa26d177
grames02 <grames02@gmail.com>
2026-03-24 21:00:27 -0600

    Deliverable 9

--- src/service.js
+++ src/service.js
@@ -51,7 +51,7 @@ app.use('*', (req, res) => {
 });
 
 // Default error handler for all exceptions and errors.
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   logger.unhandledErrorLogger(err, req);
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
 });
