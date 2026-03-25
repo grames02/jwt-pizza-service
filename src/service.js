6926240d16a758d378280bf2e4d2d1c6c23e198b
grames02 <grames02@gmail.com>
2026-03-24 23:19:38 -0600

    Deliverable 9

--- src/service.js
+++ src/service.js
@@ -50,9 +50,10 @@ app.use('*', (req, res) => {
   });
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   logger.unhandledErrorLogger(err, req);
   res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
+  next();
 });
 
 module.exports = app;
