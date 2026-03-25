9cf357b519c13a11663b6d0d07381c70abdf62db
grames02 <grames02@gmail.com>
2026-03-24 23:05:51 -0600

    Deliverable 9

--- src/service.js
+++ src/service.js
@@ -50,7 +50,7 @@ app.use('*', (req, res) => {
   });
 });
 
-app.use((err, req, res, next) => {   // add 'next'
+app.use((err, req, res) => { 
   logger.unhandledErrorLogger({
     source: 'service',              // REQUIRED by pizza-logger
     error: err,
