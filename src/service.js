b795b2a9e12b009ca5f7c390986e31c77d2deb5a
grames02 <grames02@gmail.com>
2026-03-24 23:04:17 -0600

    Deliverable 9

--- src/service.js
+++ src/service.js
@@ -50,10 +50,17 @@ app.use('*', (req, res) => {
   });
 });
 
-// Default error handler for all exceptions and errors.
-app.use((err, req, res) => {
-  logger.unhandledErrorLogger(err, req);
-  res.status(err.statusCode ?? 500).json({ message: err.message, stack: err.stack });
+app.use((err, req, res, next) => {   // add 'next'
+  logger.unhandledErrorLogger({
+    source: 'service',              // REQUIRED by pizza-logger
+    error: err,
+    req,
+  });
+
+  res.status(err.statusCode ?? 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 });
 
 module.exports = app;
