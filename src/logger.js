4c346f3cbe0f2faad90ea4e4795160aa8da7bfd2
grames02 <grames02@gmail.com>
2026-03-24 23:23:22 -0600

    Deliverable 9

--- src/logger.js
+++ src/logger.js
@@ -1,12 +1,22 @@
-const PizzaLogger = require('pizza-logger');
-
 class Logger {
   constructor(loggingConfig) {
-    // Initialize pizza-logger with your Grafana config
     this.logger = new PizzaLogger(loggingConfig);
 
-    // Expose middleware & helpers
-    this.httpLogger = this.logger.httpLogger;
+    // wrap httpLogger to avoid undefined resBody crashes
+    this.httpLogger = (req, res, next) => {
+      const originalSend = res.send;
+      res.send = (body) => {
+        try {
+          // ensure resBody is always a string or object
+          this.logger.httpLogger(req, res, body ?? '');
+        } catch (err) {
+          console.error('PizzaLogger error:', err);
+        }
+        return originalSend.call(res, body);
+      };
+      next();
+    };
+
     this.dbLogger = this.logger.dbLogger;
     this.factoryLogger = this.logger.factoryLogger;
     this.unhandledErrorLogger = this.logger.unhandledErrorLogger;
